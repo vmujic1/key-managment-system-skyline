611820c2447363913f2b1fa0102095d9e5c161fc
vmujic1 <vmujic1@etf.unsa.ba>
2024-02-19 11:50:07 +0100

    Add Room model

---
+++ Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace key_managment_system.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AccessLevelEnum AccessLevel { get; set; }
+
+        public Room()
+        {
+            
+        }
+    }
+}
