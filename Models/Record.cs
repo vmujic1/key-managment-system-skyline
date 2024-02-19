660c433dcb02729e145aa94ffc34ef49cf43906c
vmujic1 <vmujic1@etf.unsa.ba>
2024-02-19 11:53:54 +0100

    Add Record model

---
+++ Models/Record.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace key_managment_system.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Room Room { get; set; }
+
+        public Record()
+        {
+            
+        }
+
+    }
+}
