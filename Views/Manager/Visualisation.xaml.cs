5c4cf0145911189d1b85080a3d9033602fdc84d1
ehodzic6 <ehodzic6@etf.unsa.ba>
2024-02-21 22:33:01 +0100

    Add simulation for access points

--- Views/Manager/Visualisation.xaml.cs
+++ Views/Manager/Visualisation.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using key_managment_system.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,109 @@ namespace key_managment_system.Views.Manager
     /// </summary>
     public partial class Visualisation : UserControl
     {
+
+        public User currentUser { get; set; }
+        
+        
+
+        public async void UpdateDoors()
+        {
+            await Task.Delay(500);
+            
+            Qr1Btn.Visibility = Visibility.Hidden;
+            Qr2Btn.Visibility = Visibility.Hidden;
+            Qr3Btn.Visibility = Visibility.Hidden;
+            Cr1Btn.Visibility = Visibility.Hidden;
+            Cr2Btn.Visibility = Visibility.Hidden;
+            Of1Btn.Visibility = Visibility.Hidden;
+            Of2Btn.Visibility = Visibility.Hidden;
+            StaBtn.Visibility = Visibility.Hidden;
+            StrBtn.Visibility = Visibility.Hidden;
+            ToBtn.Visibility = Visibility.Hidden;
+            WsBtn.Visibility = Visibility.Hidden;
+            
+            if (currentUser.CurrentRoomId == 1)
+            {
+                Cr1Btn.Visibility= Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 2)
+            {
+                Cr2Btn.Visibility= Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 3)
+            {
+                Qr1Btn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 4)
+            {
+                Qr2Btn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 5)
+            {
+                Qr3Btn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 6)
+            {
+                StaBtn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 7)
+            {
+                Of1Btn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 8)
+            {
+                Of2Btn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 9)
+            {
+                StrBtn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 10)
+            {
+                ToBtn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if(currentUser.CurrentRoomId == 11)
+            {
+                WsBtn.Visibility = Visibility.Visible;
+                ToBtn.Visibility = Visibility.Visible;
+                Of2Btn.Visibility = Visibility.Visible;
+                Of1Btn.Visibility = Visibility.Visible;
+                StaBtn.Visibility = Visibility.Visible;
+                Qr2Btn.Visibility = Visibility.Visible;
+                Qr1Btn.Visibility = Visibility.Visible;
+                Cr1Btn.Visibility = Visibility.Visible;
+                Cr2Btn.Visibility = Visibility.Visible;
+                return;
+            }
+            else if (currentUser.CurrentRoomId == 12)
+            {
+                Qr3Btn.Visibility= Visibility.Visible;
+                StrBtn.Visibility= Visibility.Visible;
+                WsBtn.Visibility = Visibility.Visible;
+                return;
+            }
+        }
+
         public Visualisation()
         {
+            currentUser = new User();
+            currentUser.CurrentRoomId = 11;
+            
             InitializeComponent();
+            UpdateDoors();
+
+
+
         }
 
         private void strDown(object sender, RoutedEventArgs e)
@@ -94,5 +195,78 @@ namespace key_managment_system.Views.Manager
             page.Show();
         }
 
+        public void StrBtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 9;
+            UpdateDoors();
+            
+        }
+
+        public void Qr3BtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 5;
+            UpdateDoors();
+            
+        }
+
+        public void Qr2BtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 4;
+            UpdateDoors();
+        }
+
+        public void Qr1BtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 3;
+            UpdateDoors();
+        }
+
+        public void Cr2BtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 2;
+            UpdateDoors();
+        }
+        public void Cr1BtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 1;
+            UpdateDoors();
+        }
+
+        public void StaBtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 6;
+            UpdateDoors();
+        }
+
+        private void Of1BtnDown(object sender, RoutedEventArgs e)
+        {
+            
+            currentUser.CurrentRoomId = 7;
+            UpdateDoors();
+
+
+        }
+        private void Of2BtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 8;
+            UpdateDoors();
+
+            
+        }
+
+        public void ToBtnDown(object sender, RoutedEventArgs e)
+        {
+            currentUser.CurrentRoomId = 10;
+            UpdateDoors();
+        }
+
+        public void WsBtnDown(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Johny");
+            currentUser.CurrentRoomId = 12;
+            UpdateDoors();
+        }
+
+        
     }
 }
