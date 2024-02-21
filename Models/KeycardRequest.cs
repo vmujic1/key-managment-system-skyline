97801150e89ac136c1c260d62b631823bef37dc7
vmujic1 <vmujic1@etf.unsa.ba>
2024-02-22 00:25:30 +0100

    Add KeycardRequest table

---
+++ Models/KeycardRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace key_managment_system.Models
+{
+    public class KeycardRequest
+    {
+       
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        [ForeignKey("Sender")]
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
+
+        [ForeignKey("Receiver")]
+        public int ReceiverId { get; set; }
+
+        public User Receiver { get; set; }
+
+        [ForeignKey("Keycard")]
+        public int KeycardId { get; set; }
+        public Keycard Keycard { get; set; }
+
+       
+
+        
+    }
+}
