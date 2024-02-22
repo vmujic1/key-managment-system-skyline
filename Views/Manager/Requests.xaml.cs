5ff6b1580669cc85dc44eac20a41af2cbfe281d8
vmujic1 <vmujic1@etf.unsa.ba>
2024-02-22 12:49:02 +0100

    Add view for ManagerRequests

---
+++ Views/Manager/Requests.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace key_managment_system.Views.Manager
+{
+    /// <summary>
+    /// Interaction logic for Requests.xaml
+    /// </summary>
+    public partial class Requests : UserControl
+    {
+        public Requests()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteUser(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
